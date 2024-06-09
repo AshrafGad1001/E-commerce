2a24719f721ae9f580bc776e21b64396392a520d
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-06-09 08:14:44 +0300

    Feedback dection (Add Swaper)

--- js/main.js
+++ js/main.js
@@ -19,3 +19,26 @@ linkDrop.addEventListener('click', (e) => {
     subMenu.classList.toggle('active');
 });
 
+
+// ----------include Swiper files in your project----------
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+    // // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
